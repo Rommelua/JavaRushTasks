ca4b56310ee1ab67e81876dd4dfbb1b28507ef12
Leonid <rommel@ukr.net>
2020-05-05 23:52:40 +0300

    task2207

---
+++ 3.JavaMultithreading/src/com/javarush/task/task22/task2207/Solution.java
@@ -0,0 +1,80 @@
+package com.javarush.task.task22.task2207;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/*
+Обращенные слова
+*/
+public class Solution {
+    public static List<Pair> result = new LinkedList<>();
+
+    public static void main(String[] args) throws IOException {
+        String fileName = "";
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
+            fileName = reader.readLine();
+        }
+        List<String> words = Files.lines(Paths.get(fileName)).flatMap(s1 -> Arrays.stream(s1.split(" ")))
+                .collect(Collectors.toList()); // создаем List слов
+        while (words.size() > 1){
+            StringBuilder sb = new StringBuilder(words.get(0));
+            String revers = sb.reverse().toString(); //Сщхраняем перевернутое слово в переменную String
+            String word = words.remove(0); // удаляем из списка (защита от палиндромов)
+            if (words.contains(revers)) {
+                result.add(new Pair(word, revers));
+                words.remove(revers);
+            }
+        }
+       // System.out.println();
+    }
+
+    public static class Pair {
+        String first;
+        String second;
+
+        public Pair(String first, String second) {
+            this.first = first;
+            this.second = second;
+        }
+
+        public Pair() {
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+
+            Pair pair = (Pair) o;
+
+            if (first != null ? !first.equals(pair.first) : pair.first != null) return false;
+            return second != null ? second.equals(pair.second) : pair.second == null;
+
+        }
+
+        @Override
+        public int hashCode() {
+            int result = first != null ? first.hashCode() : 0;
+            result = 31 * result + (second != null ? second.hashCode() : 0);
+            return result;
+        }
+
+        @Override
+        public String toString() {
+            return first == null && second == null ? "" :
+                    first == null ? second :
+                            second == null ? first :
+                                    first.compareTo(second) < 0 ? first + " " + second : second + " " + first;
+
+        }
+    }
+
+}
+
