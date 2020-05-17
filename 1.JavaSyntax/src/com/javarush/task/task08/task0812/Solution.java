a17afc5ce4153f8482f0eb59e2446c75b08fffb3
Leonid <rommel@ukr.net>
2020-05-17 21:30:01 +0300

    task0812

--- 1.JavaSyntax/src/com/javarush/task/task08/task0812/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task08/task0812/Solution.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 /* 
@@ -17,15 +18,7 @@ public class Solution {
         for (int i = 0; i < 10; i++) {
             list.add(Integer.parseInt(reader.readLine()));
         }
-        int result = 1;
-        for (int i = 0; i < list.size()-1; i++) {
-            int counter = 1;
-            while ( i + counter < list.size() && list.get(i).equals(list.get(i + counter))) {
-                counter++;
-            }
-            if (counter > result)
-                result = counter;
-        }
+        int result = list.stream().mapToInt(i -> Collections.frequency(list, i)).max().getAsInt();
         System.out.println(result);
     }
 }
